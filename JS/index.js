4cdb616e6a8b495bddfd6281a486b7e0e93684f0
KelechiE-ART <89420505+KelechiE-ART@users.noreply.github.com>
2022-04-16 10:55:25 -0700

    sac

---
+++ JS/index.js
@@ -0,0 +1,34 @@
+console.log("It works!");
+
+$(".logo").hover(function() {
+    $('h1').fadeOut(800);
+    $('h1').fadeIn(500);
+});
+
+
+$(".card").click(function() {
+    alert("color");
+});
+
+ScrollReveal().reveal('.Examples', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+
+ScrollReveal().reveal('.mySkills', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+
+ScrollReveal().reveal('.myWork', {
+    delay: 375,
+    duration: 500,
+    reset: true
+});
+
+function fade() {
+    $('.preload').fadeOut("slow");
+    }
+    setTimeout(fade, 3000);
